e8d3524cab1bc5c052b45631856c57bab1674a8b
Ronak Shah <ronakshah2895@gmail.com>
2019-12-01 02:10:49 -0800

    added initial header and login page design

--- frontend/src/app/material-design.module.ts
+++ frontend/src/app/material-design.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatInputModule } from '@angular/material';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatInputModule
+    MatInputModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [
-  	MatInputModule
+  	MatInputModule,
+  	MatToolbarModule,
+  	MatCardModule,
+  	MatButtonModule
   ]
 })
 export class MaterialDesignModule { }
